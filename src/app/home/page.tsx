774c59aafa3d109d7a6e4a9d152a492a0417b0e1
ramesh-kunwar <rameshkunwar1110@gmail.com>
2025-01-08 19:34:42 +0545

    Added navbar

---
+++ src/app/home/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+import { Navbar } from "@/components/molecules/Navbar";
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="text-white">
+      <Navbar />
+    </div>
+  );
+};
+
+export default Home;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,11 @@ import React from "react";
 import Auth from "./auth/page";
 
 const page = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <h1 className="text-white">Home</h1>
+    </div>
+  );
 };
 
 export default page;
